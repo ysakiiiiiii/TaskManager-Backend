4ba8eb14e8b1c4ca5e4e5dbc55043e37fcc407a7
@programmingst <lucky.a.acidera@gmail.com>
2025-06-24 00:50:50 +0800

    Auth Controls

--- TaskManagerBackend/Controllers/AuthController.cs
+++ TaskManagerBackend/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TaskManagerBackend.DTOs;
+using TaskManagerBackend.Models.Domain;
 
 namespace TaskManagerBackend.Controllers
 {
@@ -7,5 +10,49 @@ namespace TaskManagerBackend.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> userManager;
+
+        public AuthController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegisterRequestDto registerRequestDto)
+        {
+            var user = new User
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username,
+                FirstName = registerRequestDto.FirstName,
+                LastName = registerRequestDto.LastName,
+            };
+
+            var identityResult = await userManager.CreateAsync(user, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRoleAsync(user, "User");
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("Registered Successfully. Please Login");
+                    }
+                }
+            }
+
+            return BadRequest(new
+            {
+                Errors = identityResult.Errors.Select(e => e.Description)
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+
+        }
     }
 }
---
+++ TaskManagerBackend/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerBackend.DTOs
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
